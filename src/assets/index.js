c455d9c8ca1e470032a997811d4ff0b8a6364650
Taufik-H <taufikrpl1@gmail.com>
2023-04-08 14:03:22 +0700

    feat:i change image name

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,4 @@ export { default as logo } from "../assets/logo.png";
 export { default as goPage } from "../assets/go.svg";
 export { default as hero1 } from "../assets/heroimage1.png";
 export { default as hero3 } from "../assets/heroimage3.png";
-export { default as hero2 } from "../assets/heroimage2.png";
+export { default as hero2 } from "../assets/hero2.png";
